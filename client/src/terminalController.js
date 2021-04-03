7e07b64b6c8ccc7773755f5abb17518142a4a868
edmilson-dk <droidvidaboa@gmail.com>
2021-04-03 10:12:02 -0300

    feat: criado funcoes iniciais para criar os componentes da interface cli do chat

--- client/src/terminalController.js
+++ client/src/terminalController.js
@@ -1,7 +1,45 @@
+import ComponentsBuilder from "./components.js";
+
 export default class TerminalController {
   constructor() {}
 
+  #onInputReceived(eventEmitter) {
+    return function () {
+      const message = this.getValue();
+     
+      this.clearValue();
+    }
+  }
+
+  #onMessageReceived({ screen, chat }) {
+    return msg => {
+      const { username, message } = msg;
+
+      chat.addItem(`{bold}${username}{/}: ${message}`);
+      screen.render();
+    }
+  }
+
+  #registerEvents(eventEmitter, components) {
+    eventEmitter.on('message:received', this.#onMessageReceived(components));
+  }
+
   async initializeTable(eventEmitter) {
-    console.log('Inicializou!');
+    const components = new ComponentsBuilder()
+      .setScreen({ title: 'DK λ - Chat'})
+      .setLayoutComponent()
+      .setInputComponent(this.#onInputReceived(eventEmitter))
+      .setChatComponent()
+      .build();
+
+    this.#registerEvents(eventEmitter, components);
+
+    components.input.focus();
+    components.screen.render();
+
+    setInterval(() => {
+      eventEmitter.emit('message:received', { message: 'Hello world!!', username: 'dk'});
+      eventEmitter.emit('message:received', { message: 'Hello', username: 'julia'});
+    }, 2000);
   }
 }
