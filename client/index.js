e41a6bac1bab5c72d621e826cea02dc665373691
edmilson-dk <droidvidaboa@gmail.com>
2021-04-03 19:19:50 -0300

    feat: adicionado instancia da classe cli config e process.argv para pegar comandos digitados na cli

--- client/index.js
+++ client/index.js
@@ -1,7 +1,11 @@
 import Events from "events";
+import CliConfig from "./src/cliConfig.js";
 
 import TerminalController from "./src/terminalController.js";
 
+const [ nodePath, filePath, ...commands ] = process.argv;
+
+const config = CliConfig.parseArguments(commands);
 const componentEmitter = new Events();
 
 const controller = new TerminalController();
