f4500cfaf696f47102dd8800c264a4b2660d86f9
edmilson-dk <droidvidaboa@gmail.com>
2021-04-03 21:15:45 -0300

    feat: added event emitter new connection handler

--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,6 @@
 import Event from 'events';
+import { constants } from './constants.js';
+import Controller from './controller.js';
 
 import SocketServer from "./socket.js";
 
@@ -6,7 +8,49 @@ const PORT = process.env.PORT || 9898;
 
 const eventEmitter = new Event();
 
+// async function testServer() {
+//   const options = {
+//     port: 9898,
+//     host: 'localhost',
+//     headers: {
+//       Connection: 'Upgrade',
+//       Upgrade: 'webSocket',
+//     }
+//   }
+
+//   const http = await import('http');
+//   const req = http.request(options);
+//   req.end();
+
+//   req.on('upgrade', (res, socket) => {
+//     socket.on('data', data => {
+//       console.log('Client received: ', data.toString());
+//     });
+
+//     setInterval(() => {
+//       socket.write('hello server!');
+//     }, 500);
+//   });
+// }
+
 const socketServer = new SocketServer({ port: PORT });
 const server = await socketServer.initialize(eventEmitter);
 
 console.log('socket server is running at: ', server.address().port);
+const controller = new Controller({ socketServer });
+
+eventEmitter.on(
+  constants.events.NEW_USER_CONNECTED, 
+  controller.onNewConnection.bind(controller)
+);
+
+// eventEmitter.on(constants.events.NEW_USER_CONNECTED, socket => {
+//   console.log('new connected: ', socket.id);
+
+//   socket.on('data', data => {
+//     console.log('server received: ', data.toString());
+//     socket.write('hello client!');
+//   });
+// })
+
+// await testServer();
