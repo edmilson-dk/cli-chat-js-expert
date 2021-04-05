68969072bf11d7b3c112a19e782f43ec3ba2c486
edmilson-dk <droidvidaboa@gmail.com>
2021-04-05 09:14:42 -0300

    feat: added event manager instance

--- client/index.js
+++ client/index.js
@@ -1,6 +1,7 @@
 import Events from "events";
 
 import CliConfig from "./src/cliConfig.js";
+import EventManager from "./src/eventManager.js";
 import SocketClient from "./src/socket.js";
 
 import TerminalController from "./src/terminalController.js";
@@ -10,8 +11,16 @@ const [ nodePath, filePath, ...commands ] = process.argv;
 const componentEmitter = new Events();
 const config = CliConfig.parseArguments(commands);
 const socketClient = new SocketClient(config);
+const eventManager = new EventManager({ componentEmitter, socketClient });
 
 await socketClient.initialize();
 
-// const controller = new TerminalController();
-// await controller.initializeTable(componentEmitter);
+const data = {
+  roomId: config.room,
+  userName: config.username,
+};
+
+eventManager.joinRoomAndWaitFortMessage(data);
+
+const controller = new TerminalController();
+await controller.initializeTable(componentEmitter);
