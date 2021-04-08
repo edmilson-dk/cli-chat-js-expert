627ef88b6f5d8f557fd3ac55f14fe65f815ad1d2
edmilson-dk <droidvidaboa@gmail.com>
2021-04-08 17:10:46 -0300

    feat: created disconnecte user method

--- client/src/eventManager.js
+++ client/src/eventManager.js
@@ -23,6 +23,14 @@ export default class EventManager {
     this.#updateUsersComponent();
   }
 
+  disconnectUser(user) {
+    const { userName, id } = user;
+    this.#allUsers.delete(id);
+
+    this.#updateActivityLogComponent(`${userName} logout!`);
+    this.#updateUsersComponent();
+  }
+
   newUserConnected(message) {
     const user = message;
     this.#allUsers.set(user.id, user.userName);
